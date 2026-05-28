5391866f4e837dc8f5f89757af14dc639c572857
Nathanael <nathanaelsta.catalina0@gmail.com>
2026-05-29 02:47:53 +0800

    Updated project

--- client/src/components/Map.tsx
+++ client/src/components/Map.tsx
@@ -181,6 +181,6 @@ export function MapView({
   }, [init]);
 
   return (
-    <div ref={mapContainer} className={cn("w-full h-[500px]", className)} />
+    <div ref={mapContainer} className={cn("w-full h-[500px]", className)} style={{ isolation: "isolate", position: "relative", zIndex: 0, contain: "layout style" }} />
   );
 }
