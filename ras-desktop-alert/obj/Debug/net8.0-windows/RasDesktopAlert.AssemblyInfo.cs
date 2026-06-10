5f0caa9966608752b1af169eaa2f5383d8ce73e6
Nathanael <nathanaelsta.catalina0@gmail.com>
2026-06-11 02:22:08 +0800

    Updated project

--- ras-desktop-alert/obj/Debug/net8.0-windows/RasDesktopAlert.AssemblyInfo.cs
+++ ras-desktop-alert/obj/Debug/net8.0-windows/RasDesktopAlert.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Five Stones RAS Desktop Alert - Flashes red/blue on emergency alerts")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.1.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.1.0+462de071b66822dce726a565725c032bbb9871cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.1.0+993a4a5eee3fd17812dc7e5fb8d170f6829a9a7b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Response Activation System - Desktop Alert")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FiveStonesRASAlert")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.1.0.0")]
